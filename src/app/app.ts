a63e350d23fcaf23701437b332ac3d48ebbd974d
DeveloperRucel07 <tsafackrucel@gmail.com>
2025-10-15 21:03:36 +0200

    add location component

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import {Footer} from './shared/components/footer/footer'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainContent, Footer],
+  imports: [RouterOutlet, MainContent, Footer, ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/main-content/main-content.ts
+++ src/app/main-content/main-content.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landingPage/landingPage.component';
 import { HowToOrder } from './how-to-order/how-to-order/how-to-order';
 import { OurMenu } from './our-menu/our-menu/our-menu';
+import { Location } from './location/location';
 
 @Component({
   selector: 'app-main-content',
-  imports: [CommonModule, LandingPageComponent, HowToOrder, OurMenu ],
+  imports: [CommonModule, LandingPageComponent, HowToOrder, OurMenu, Location],
   templateUrl: './main-content.html',
   styleUrl: './main-content.scss'
 })
